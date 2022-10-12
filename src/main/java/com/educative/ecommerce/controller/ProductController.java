6c64cabac5d4b63627160bc2c164e8edaa112e49
ChandruJHL <chandrujhl4@gmail.com>
2022-10-12 23:18:09 +0530

    Ecommerce website initial code

---
+++ src/main/java/com/educative/ecommerce/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.educative.ecommerce.controller;
+
+import com.educative.ecommerce.service.CategoryService;
+import com.educative.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+    @Autowired
+    CategoryService categoryService;
+}
---
+++ src/main/java/com/educative/ecommerce/dto/ProductDto.java
@@ -0,0 +1,4 @@
+package com.educative.ecommerce.dto;
+
+public class ProductDto {
+}
